c089845c72b8a0bbd71d68becbf013f87d8e26e3
Hiep Nguyen <tom.hiepnguyen@gmail.com>
2021-03-26 18:05:32 +0700

    Updates

---
+++ component---src-pages-page-2-tsx-c0cb60522414351739ef.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[473],{1922:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5444),r=a(2755),c=a(9470);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-c0cb60522414351739ef.js.map
