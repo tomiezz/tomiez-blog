3d7b6c7f92d4ee8f37471181e46ce992ee32ab30
Hiep Nguyen <tom.hiepnguyen@gmail.com>
2021-05-14 22:48:57 +0700

    add n-progress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,15 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
