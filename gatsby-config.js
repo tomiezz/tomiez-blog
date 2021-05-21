53d47abac25f43aa494ee4cb32cd2dfbe4afe9d4
Hiep Nguyen <tom.hiepnguyen@gmail.com>
2021-05-21 16:58:44 +0700

    improve: update project manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,13 +67,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Tomiez blog`,
+        short_name: `Tomiez.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/site-image.png`, // This path is relative to the root of the site.
+        background_color: `#333333`,
+        theme_color: `#333333`,
+        display: `fullscreen`,
+        icon: `./static/site-image.png`, // This path is relative to the root of the site.
         include_favicon: false,
       },
     },
